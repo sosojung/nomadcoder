fdc9afee3d304c85fbd6ceeaeba080235c549dfd
sosojung <sojungmoon6@gmail.com>
2024-07-24 21:56:41 +0900

    real 3.4 	modified:   momentum/app.js

--- momentum/app.js
+++ momentum/app.js
@@ -1,7 +1,19 @@
 const title = document.querySelector(".hello:first-child h1");
 
+console.dir(title);
+
 function handleTitleClick() {
     title.style.color = "blue";
 }
 
-title.addEventListener("click", handleTitleClick)
+function handleMouseEnter() {
+    title.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+    title.innerText = "Mouse is gone!"
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
