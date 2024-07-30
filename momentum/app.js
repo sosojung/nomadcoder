6a67ffe7833801360157f51a52acf61a2f8a470c
sosojung <sojungmoon6@gmail.com>
2024-07-30 21:19:58 +0900

    4.0 	modified:   momentum/app.js 	modified:   momentum/index.html 	modified:   momentum/style.css

--- momentum/app.js
+++ momentum/app.js
@@ -1,8 +1,8 @@
-const h1 = document.querySelector(".hello:first-child h1");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
-function handleTitleClick() {
-    h1.classList.toggle("clicked");
+function onLoginBtnClick() {
+    console.log("Hello", loginInput.value);
 }
 
-h1.addEventListener("click", handleTitleClick);
- 
+loginButton.addEventListener("click", onLoginBtnClick);
