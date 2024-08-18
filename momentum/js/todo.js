482d8ed8a07a8d58574a2658a731f174fd1d58bb
sosojung <sojungmoon6@gmail.com>
2024-08-18 22:24:51 +0900

    7.1 	modified:   momentum/js/todo.js

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -2,10 +2,19 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function painToDo(newTodo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newTodo;
+    toDoList.appendChild(li);
+}
+
 function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value
     toDoInput.value = "";
+    painToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
